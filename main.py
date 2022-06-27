775f83f63d3c3cd1208dbb3c86697aa9d06bb910
Naman <f20180435@pilani.bits-pilani.ac.in>
2022-06-27 21:00:36 +0530

    extract tables from websites

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# https://www.freecodecamp.org/news/automate-your-life-with-python/
 import pandas as pd
 import numpy as np
 
@@ -11,11 +7,12 @@ np.set_printoptions(linewidth=320)
 pd.set_option('display.max_columns', 10)
 
 # Extract tables from websites
-#a = pd.read_html('https://en.wikipedia.org/wiki/The_Avengers_(2012_film)')
-#print(a[1])
+a = pd.read_html('https://en.wikipedia.org/wiki/The_Avengers_(2012_film)')
+print(a[1])
 
 # Extract tables from pdfs
 import camelot
+
 tables = camelot.read_pdf('Resume_NamanAgarwal_BITSP.pdf', pages='1')
 print(tables)
 
